60328a49a0af99aea400410126ed1e35599a07bd
izinii <121061445+izinii@users.noreply.github.com>
2024-11-19 15:42:35 +0100

    ADD component admin-film

---
+++ app-cinevision-front/src/app/admin-film/admin-film.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-film',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-film.component.html',
+  styleUrl: './admin-film.component.css'
+})
+export class AdminFilmComponent {
+
+}
