1df5a9eff76b7f5c0fc0181fc3cf6733b03ed3e6
LisaNaccache <naccachelisa@gmail.com>
2024-12-15 19:35:33 +0100

    Add Home Page

--- app-cinevision-front/src/app/film/film.component.ts
+++ app-cinevision-front/src/app/film/film.component.ts
@@ -1,12 +1,58 @@
 import { Component } from '@angular/core';
+import {FilmService} from '../Admin/Film/film.service';
+import {CommonModule, NgFor, NgIf} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-film',
   standalone: true,
-  imports: [],
+  imports: [
+    NgFor,
+    NgIf,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterLink
+  ],
   templateUrl: './film.component.html',
   styleUrl: './film.component.css'
 })
 export class FilmComponent {
+  films: any[] = [];
+  isLoading = true;
+  hasError = false;
 
+  constructor(private filmService: FilmService) {}
+
+  ngOnInit(): void {
+    this.loadFilms();
+  }
+
+  loadFilms(): void {
+    this.filmService.getAllFilms().subscribe(
+      (data: any[]) => {
+        this.films = data.map((filmArray) => ({
+          id: filmArray[0],
+          title: filmArray[1],
+          language: filmArray[2],
+          description: filmArray[3],
+          popularity: filmArray[4],
+          releaseDate: filmArray[5],
+          runtime: filmArray[6],
+          status: filmArray[7],
+          voteCount: filmArray[8],
+          voteAverage: filmArray[9],
+          posterUrl: filmArray[10],
+          trailerUrl: filmArray[11],
+        }));
+        this.isLoading = false;
+      },
+      (error) => {
+        console.error('Error loading films:', error);
+        this.hasError = true;
+        this.isLoading = false;
+      }
+    );
+  }
 }
