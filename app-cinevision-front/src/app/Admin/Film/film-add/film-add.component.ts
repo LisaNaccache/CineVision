c34e210b2994e29674aa9ad2057dcebf0b58b2df
LisaNaccache <naccachelisa@gmail.com>
2024-12-15 19:34:20 +0100

    ADD Add Film

--- app-cinevision-front/src/app/Admin/Film/film-add/film-add.component.ts
+++ app-cinevision-front/src/app/Admin/Film/film-add/film-add.component.ts
@@ -1,12 +1,81 @@
 import { Component } from '@angular/core';
+import {FilmService} from '../film.service';
+import {Router, RouterLink} from '@angular/router';
+import {CommonModule, NgFor, NgIf} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-film-add',
   standalone: true,
-  imports: [],
+  imports: [
+    NgFor,
+    NgIf,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterLink,
+  ],
   templateUrl: './film-add.component.html',
   styleUrl: './film-add.component.css'
 })
 export class FilmAddComponent {
 
+  film: any = {
+    title: '',
+    language: '',
+    description: '',
+    popularity: null,
+    releaseDate: '',
+    runtime: null,
+    status: '',
+    voteCount: null,
+    voteAverage: null,
+    posterUrl: '',
+    trailerUrl: '',
+  };
+
+  isLoading = false;
+  hasError = false;
+
+  constructor(private filmService: FilmService, private router: Router) {}
+
+  onFormSubmit(): void {
+    this.isLoading = true;
+
+    const formatDate = (date: string): string => {
+      const d = new Date(date);
+      const year = d.getFullYear();
+      const month = String(d.getMonth() + 1).padStart(2, '0'); // Mois format 2 chiffres
+      const day = String(d.getDate()).padStart(2, '0'); // Jour format 2 chiffres
+      return `${year}-${month}-${day}`;
+    };
+
+    const newFilm = {
+      title: this.film.title,
+      original_language: this.film.language || null,
+      overview: this.film.description || null,
+      popularity: this.film.popularity || null,
+      release_date: formatDate(this.film.releaseDate), // Conversion au format attendu
+      runtime: this.film.runtime || null,
+      status: this.film.status || null,
+      vote_count: this.film.voteCount || null,
+      vote_average: this.film.voteAverage || null,
+      link_poster: this.film.posterUrl || null,
+      link_trailer: this.film.trailerUrl || null,
+    };
+
+    this.filmService.addFilm(newFilm).subscribe(
+      (response) => {
+        alert('Film added successfully!');
+        console.log('New film:', response);
+        this.router.navigate(['/admin/film']); // Redirige vers la liste des films
+        this.isLoading = false;
+      },
+      (error) => {
+        console.error('Error adding film:', error);
+        this.hasError = true;
+        this.isLoading = false;
+      }
+    );
+  }
 }
