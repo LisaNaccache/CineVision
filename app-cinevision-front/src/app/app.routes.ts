830c74acb90cfe06b8aba7fcb627493b38948b8e
LisaNaccache <naccachelisa@gmail.com>
2024-12-16 09:14:46 +0100

    DELETE superflue

--- app-cinevision-front/src/app/app.routes.ts
+++ app-cinevision-front/src/app/app.routes.ts
@@ -4,9 +4,6 @@ import {FilmEditComponent} from './Admin/Film/film-edit/film-edit.component';
 import {FilmAddComponent} from './Admin/Film/film-add/film-add.component';
 import {ConnectionComponent} from './connection/connection.component';
 import {FilmListComponent} from './Admin/Film/film-list/film-list.component';
-import {ActeurListComponent} from './Admin/Acteur/acteur-list/acteur-list.component';
-import {ActeurEditComponent} from './Admin/Acteur/acteur-edit/acteur-edit.component';
-import {ActeurAddComponent} from './Admin/Acteur/acteur-add/acteur-add.component';
 import {CategorieAddComponent} from './Admin/Categorie/categorie-add/categorie-add.component';
 import {CategorieEditComponent} from './Admin/Categorie/categorie-edit/categorie-edit.component';
 import {CategorieListComponent} from './Admin/Categorie/categorie-list/categorie-list.component';
@@ -24,21 +21,19 @@ export const routes: Routes = [
   { path: 'inscription', component: InscriptionComponent },
   { path: 'connection', component: ConnectionComponent },
   { path: 'admin/film/add', component: FilmAddComponent },
-  { path: 'admin/film/edit/:id', component: FilmEditComponent }, // Correction ici
+  { path: 'admin/film/edit/:id', component: FilmEditComponent },
   { path: 'admin/film', component: FilmListComponent },
+  { path: '', component: FilmComponent },
   { path: 'client/film', component: FilmComponent },
   { path: 'client/film/:id', component: FilmDetailComponent },
-  { path: 'admin/acteur/add', component: ActeurAddComponent },
-  { path: 'admin/acteur/edit/:id', component: ActeurEditComponent }, // Correction ici
-  { path: 'admin/acteur', component: ActeurListComponent },
   { path: 'admin/categorie/add', component: CategorieAddComponent },
-  { path: 'admin/categorie/edit/:id', component: CategorieEditComponent }, // Correction ici
+  { path: 'admin/categorie/edit/:id', component: CategorieEditComponent },
   { path: 'admin/categorie', component: CategorieListComponent },
   { path: 'admin/cinema/add', component: CinemaAddComponent },
-  { path: 'admin/cinema/edit/:id', component: CinemaEditComponent }, // Correction ici
+  { path: 'admin/cinema/edit/:id', component: CinemaEditComponent },
   { path: 'admin/cinema', component: CinemaListComponent },
   { path: 'admin/review', component: ReviewListComponent },
   { path: 'admin/session-film/add', component: SessionFilmAddComponent },
-  { path: 'admin/session-film/edit/:id', component: SessionFilmEditComponent }, // Correction ici
+  { path: 'admin/session-film/edit/:id', component: SessionFilmEditComponent },
   { path: 'admin/session-film', component: SessionFilmListComponent },
 ];
